f48464009bdee2f6f148eca05b41c41a33ac4a6a
Dewansh Yadav <dewanshyadaw8@gmail.com>
2025-11-07 15:31:10 +0530

    only names sent after encryption

--- src/components/CometChatSearch/useCometChatSearchMessagesList.tsx
+++ src/components/CometChatSearch/useCometChatSearchMessagesList.tsx
@@ -16,7 +16,7 @@ import { CometChatButton } from "../BaseComponents/CometChatButton/CometChatButt
 import { CometChatMentionsFormatter, CometChatTextHighlightFormatter } from "../../formatters";
 import { CometChatUIKitLoginListener } from "../../CometChatUIKit/CometChatUIKitLoginListener";
 import {  hasLink, hasValidMessageSearchCriteria, isMonthDifferent } from "../../utils/SearchUtils";
-import { isMessageSentByMe, isURL, sanitizeCalendarObject } from "../../utils/util";
+import { encryptName, isMessageSentByMe, isURL, sanitizeCalendarObject } from "../../utils/util";
 import { CometChatUIKit } from "../../CometChatUIKit/CometChatUIKit";
 import { MessageUtils } from "../../utils/MessageUtils";
 import { MessagesDataSource } from "../../utils/MessagesDataSource";
@@ -869,7 +869,8 @@ const getMessageTitle = useCallback((message: CometChat.BaseMessage): string => 
     }
   
     if (searchKeyword && searchKeyword.trim() !== "") {
-      builder = builder.setSearchKeyword(searchKeyword);
+      const trimmedKeyword = searchKeyword.trim();
+      builder = builder.setSearchKeyword(encryptName(trimmedKeyword));
     }
   
     if (uid) {
